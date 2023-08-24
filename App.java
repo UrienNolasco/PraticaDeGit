92f12e453e8e359e084458dc52c353344f1d424c
Urien Nolasco <urien.dragon@gmail.com>
2023-08-24 11:50:42 -0300

    Adicionando metodo F

--- App.java
+++ App.java
@@ -12,6 +12,12 @@ public class App {
         lista.add(2);
         lista.add(5);
 
+        ArrayList<Integer> lista2 = new ArrayList<>();
+        lista2.add(2);
+        lista2.add(5);
+        lista2.add(7);
+        lista2.add(8);
+
         Integer elemento = 2;
         int ocorrencias = nOcorrencias(lista, elemento);
         System.out.println("Ocorrências de " + elemento + ": " + ocorrencias);
@@ -21,6 +27,9 @@ public class App {
 
         int numeroRepetidos = nroRepeat(lista);
         System.out.println("Número de elementos repetidos: " + numeroRepetidos);
+
+        ArrayList<Integer> intersecao = intersect(lista, lista2);
+        System.out.println("Interseção entre lista e lista2: " + intersecao);
     }
 
     public static int nOcorrencias(ArrayList<Integer> l, Integer el) {
@@ -53,5 +62,23 @@ public class App {
     public static int nroRepeat(ArrayList<Integer> l) {
         ArrayList<Integer> elementosRepetidos = listRepeat(l);
         return elementosRepetidos.size();
+    }
+
+    public static ArrayList<Integer> intersect(ArrayList<Integer> l1, ArrayList<Integer> l2) {
+        ArrayList<Integer> intersection = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (Integer num : l1) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for (Integer num : l2) {
+            if (map.containsKey(num) && map.get(num) > 0) {
+                intersection.add(num);
+                map.put(num, map.get(num) - 1);
+            }
+        }
+
+        return intersection;
     }
 }
